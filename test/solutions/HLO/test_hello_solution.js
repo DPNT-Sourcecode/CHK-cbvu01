c0909a0c070762869ddd1850a9d2b00096be0468
sbx_user1051 <sbx_user1051@169.254.80.233>
2026-02-06 18:02:46 +0000

    Fri Feb 06 18:02:46 UTC 2026

--- test/solutions/HLO/test_hello_solution.js
+++ test/solutions/HLO/test_hello_solution.js
@@ -1,9 +1,29 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import HelloSolution from "../../../lib/solutions/HLO/hello_solution.js"
+import HelloSolution from "../../../lib/solutions/HLO/hello_solution.js";
 
 describe("Hello World challenge: Return the correct greeting", function () {
   it("should return 'Hello there Vlad!'", function () {
-    assert.strictEqual(new HelloSolution().hello("Vlad"), "Hello there Vlad!")
+    assert.strictEqual(new HelloSolution().hello("Vlad"), "Hello there Vlad!");
+  });
+
+  it("should return error if parameter is not defined or string", function () {
+    assert.throws(() => {
+      (new HelloSolution().hello(),
+        {
+          name: "Error",
+          message:
+            "Please provide a valid parameter (Parameter must be a string!).",
+        });
+    });
+    assert.throws(() => {
+      (new HelloSolution().hello(123),
+        {
+          name: "Error",
+          message:
+            "Please provide a valid parameter (Parameter must be a string!).",
+        });
+    });
   });
 });
+
